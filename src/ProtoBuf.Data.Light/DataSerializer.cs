178a5e4cc5f5ebc1a87c5fae0cbef7c0caf73220
Arjen Post <mail@arjenpost.nl>
2014-08-24 08:02:37 +0200

    DataSerializer documentation

--- src/ProtoBuf.Data.Light/DataSerializer.cs
+++ src/ProtoBuf.Data.Light/DataSerializer.cs
@@ -8,6 +8,13 @@ namespace ProtoBuf.Data.Light
 {
     public static class DataSerializer
     {
+        /// <summary>
+        /// Serializes the <see cref="IDataReader"/> to the given stream.
+        /// </summary>
+        /// <param name="stream">The stream to which the <see cref="IDataReader"/> is to be serialized.</param>
+        /// <param name="reader">The <see cref="IDataReader"/> to serialize.</param>
+        /// <exception cref="ArgumentNullException">The stream is null. -or-The reader is null.</exception>
+        /// <exception cref="UnsupportedDataTypeException">A data type is not supperted.</exception>
         public static void Serialize(Stream stream, IDataReader reader)
         {
             Throw.IfNull(stream, "stream");
@@ -16,6 +23,12 @@ namespace ProtoBuf.Data.Light
             ProtoBufDataWriter.WriteReader(stream, reader);
         }
 
+        /// <summary>
+        /// Deserializes the specified stream into an <see cref="IDataReader"/>.
+        /// </summary>
+        /// <param name="stream">The stream from which to deserialize the <see cref="IDataReader"/>.</param>
+        /// <returns>The <see cref="IDataReader"/>.</returns>
+        /// <exception cref="InvalidDataException">The stream has an unexpected format.</exception>
         public static IDataReader Deserialize(Stream stream)
         {
             Throw.IfNull(stream, "stream");
