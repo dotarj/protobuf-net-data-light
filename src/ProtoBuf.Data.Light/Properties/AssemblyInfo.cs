600698abf9c5a038a0f58022cd55bf8332512b24
Arjen Post <mail@arjenpost.nl>
2014-09-02 20:50:24 +0200

    AssemblyInfo and nuspec update

--- src/ProtoBuf.Data.Light/Properties/AssemblyInfo.cs
+++ src/ProtoBuf.Data.Light/Properties/AssemblyInfo.cs
@@ -2,13 +2,18 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("protobuf-net-data-light")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("ProtoBuf Data Light: a lightweight IDataReader serializer. This library serializes an IDataReader to a binary format using Marc Gravell's protobuf-net.")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: AssemblyCompany("Arjen Post")]
 [assembly: AssemblyProduct("ProtoBuf Data Light: a lightweight IDataReader serializer")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-beta")]
 [assembly: ComVisible(false)]
